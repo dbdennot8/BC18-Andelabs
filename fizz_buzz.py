3f067a8b2635c54fbfb7896a8d34cb49a1bc1384
dbdennot8 <dennisbmithamo@gmail.com>
2017-05-09 09:09:44 +0300

    Python FizzBuzz function

---
+++ fizz_buzz.py
@@ -0,0 +1,15 @@
+def fizz_buzz(num):
+
+	if num % 3 == 0 and num % 5 == 0:
+		return "FizzBuzz"
+
+	elif num % 3 == 0:
+		return "Fizz"
+
+	elif num % 3 == 0:
+		return "Buzz"
+
+	else:
+		return num
+
+print fizz_buzz(9)
